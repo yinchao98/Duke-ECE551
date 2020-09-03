69cd56712eaa4fed4399b74c997f194d8a0bb120
ys322 <ys322@duke.edu>
2020-09-03 10:19:02 +0800

    feat: 017 submission of Yinchao

--- 017_squares/squares.c
+++ 017_squares/squares.c
@@ -1,12 +1,33 @@
+int maximum(int x, int y) {
+  if (x > y) {
+    return x;
+  }else {
+    return y;
+  }
+}
+int isBetweenAndEqual(int x, int y, int lower, int upper, int a, int b) {
+  if ((x > lower && x <= upper) && (y == a || y == b)) {
+    return 1;
+  }else {
+    return 0;
+  }
+}
+int  isLessAndEqual(int x, int y, int upper, int a, int b) {
+  if ((x < upper) && (y == a || y == b)) {
+    return 1;
+  }else {
+    return 0;
+  }
+}
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int w = maximum(size1, (x_offset + size2));
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h = maximum(size1, (y_offset + size2));
   //count from 0 to h. Call the number you count with y
-
+  for (int y = 0; y < h; y++) {
     //count from 0 to w. Call the number you count with x
-
+    for (int x = 0; x < w; x++) {
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -14,6 +35,18 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
+      if (isBetweenAndEqual(x, y, x_offset, x_offset +size2, y_offset, y_offset + size2 - 1) || isBetweenAndEqual(y, x, y_offset, y_offset + size2, x_offset, x_offset + size2 -1)) {
+	printf("*");
+      }else {
+	if (isLessAndEqual(x, y, size1, 0, size1-1) || isLessAndEqual(y, x, size1, 0, size1-1)) {
+	  printf("#");
+	}else {
+	  printf(" ");
+	}
+      }
+    }
+    printf("\n");
+  }
 
       //if not,
       // check if EITHER
