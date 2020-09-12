95188d345cac2a213dd160e99e01c5bc12d2f5ba
ys322 <ys322@duke.edu>
2020-09-12 13:26:22 +0800

    feat: 042 submission of Yinchao

--- 042_bits_arr/numToBits.c
+++ 042_bits_arr/numToBits.c
@@ -11,7 +11,17 @@ int getNthBit(uint32_t number, int bit) {
 }
 
 void numToBits(uint32_t * nums, int nNums, int * bits, int nBits) {
-
+  if ((nNums * sizeof(nNums) * 8) != nBits) {
+    printf("Invalid call to numToBits! nBits is %d, nNums is %d\n", nBits, nNums);
+  } else {
+    for(size_t i = 0; i < nNums; i++) {
+      size_t index = sizeof(nNums) * 8 - 1;
+      for(size_t j = 0; j < sizeof(nNums) * 8; j++) {
+        bits[i * sizeof(nNums) * 8 + j] = getNthBit(*(nums + i), index);
+        index--;
+      }
+    }
+  }
 }
 
 void doTest(uint32_t * nums, int n) {
