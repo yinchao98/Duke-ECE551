54a2cdc2c08f939041d88a1072fbe24e0767ace6
ys322 <ys322@duke.edu>
2020-09-13 15:46:28 +0800

    feat: 047 submission of Yinchao

---
+++ 047_break_encr/break.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void freqCount(FILE* f, int* alphaCount){
+	int c;
+	while ((c = fgetc(f)) != EOF) {
+		if (isalpha(c)) {
+			alphaCount[c - 'a'] += 1;
+		}
+	}
+}
+
+int getMax(int* alphaCount) {
+	int maxCount  = alphaCount[0];
+	int maxIndex = 0;
+	for(int i = 1; i < 26; i++) {
+		if (alphaCount[i] > maxCount) {
+			maxCount = alphaCount[i];
+			maxIndex = i;
+		}
+	}
+	return maxIndex;
+}
+
+int decrypt(int maxIndex) {
+	int key = maxIndex - ('e' - 'a');
+	return key;
+}
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: break inputFileName\n");
+		return EXIT_FAILURE;
+	}
+	FILE* f = fopen(argv[1], "r");
+	if (f == NULL) {
+		fprintf(stderr, "could not open the file\n");
+	}
+	int alphaCount[26] = {0};
+	freqCount(f, alphaCount);
+	int maxIndex = getMax(alphaCount);
+	int key = decrypt(maxIndex);
+	printf("%d\n", key);
+	if (fclose(f) != 0) {
+		fprintf((stderr), "Failed to close the input file!\n");
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
