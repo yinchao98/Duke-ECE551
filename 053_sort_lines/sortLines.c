7876a3da5f3f216f29a188509def8c564ead9ec3
ys322 <ys322@duke.edu>
2020-09-16 21:36:54 +0800

    feat: 053 submission of Yinchao

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -15,10 +15,51 @@ int stringOrder(const void * vp1, const void * vp2) {
 void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
+//This function print data and free the previous allocation
+void printData(char** data, size_t count) {
+  for (size_t j = 0; j < count; j++) {
+      printf("%s", *(data + j));
+      free(*(data + j));
+    }
+}
 
 int main(int argc, char ** argv) {
-  
-  //WRITE YOUR CODE HERE!
-  
+  char** data = NULL;
+  char* curr = NULL;
+  size_t sz = 0;
+  size_t count = 0;
+  // if there's no input file, use standard input
+  if (argc == 1) {
+    while (getline(&curr,&sz, stdin) != EOF) {
+      data = realloc(data, (count + 1) * sizeof(*data));
+      data[count] = curr;
+      curr = NULL;
+      count++;
+    }
+    free(curr);
+    sortData(data, count);
+    printData(data, count);
+  } else { 
+    for (size_t i = 1; i < argc; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        perror("Could not open file");
+        return EXIT_FAILURE;
+      }
+      while (getline(&curr,&sz, f) >= 0) {
+        data = realloc(data, (count + 1) * sizeof(*data));
+        data[count] = curr;
+        curr = NULL;
+        count++;
+      }
+      free(curr);
+      sortData(data, count);
+      printData(data, count);
+      if (fclose(f) != 0) {
+        perror("Failed to close the input file!");
+        return EXIT_FAILURE;
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
