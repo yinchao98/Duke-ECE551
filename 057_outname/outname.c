2d6ab7c429a23c5d1ed7748d8de7b772ae3a3cca
ys322 <ys322@duke.edu>
2020-09-23 11:55:04 +0800

    feat: 057 submission of Yinchao

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -5,4 +5,15 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+	char* suffix = ".counts";
+
+	int inputLen = strlen(inputName);
+	int suffixLen = strlen(suffix);
+
+	char* outputFileName = malloc((inputLen + suffixLen + 1) * sizeof(*outputFileName));
+	strcpy(outputFileName, inputName);
+	strcat(outputFileName, suffix);
+
+	return outputFileName;
+
 }
