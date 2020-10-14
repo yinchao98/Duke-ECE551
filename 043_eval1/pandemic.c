0f006d40c7bf1bc73eed57fa6d2ba112bacb1d94
ys322 <ys322@duke.edu>
2020-10-14 13:53:13 +0800

    feat: 043 eval

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -17,6 +17,54 @@ int isNumber(char c) {
   return 0;
 }
 
+// check if the name length is < 64
+void checkName(char* start, char* end) {
+  if((end - start) > 63) {
+    fprintf(stderr, "name length is too long!\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+// copy the name to dest
+void copyName(char* start, char* end, char* dest) {
+  size_t i = 0;
+  while(start != end) {
+    dest[i] = *start;
+    i++;
+    start++;
+  }
+  // add null terminator
+  dest[i] = '\0';
+}
+
+// check if the string from start to end is legal
+void checkPop(char* start, char* end) {
+  while(start != end) {
+    if(!isNumber(*start)) {
+      fprintf(stderr, "The population is invalid!\n");
+      exit(EXIT_FAILURE);
+    }
+    start++;
+  }
+}
+
+// convert the string to uint64
+unsigned long convertPop(char* start) {
+  unsigned long population = strtoul(start, NULL, 10);
+  // check whether the population is out of the bound
+  if(errno == ERANGE) {
+    fprintf(stderr, "The population is too large!\n");
+    exit(EXIT_FAILURE);
+  }
+  errno = 0;
+  // check whether the population is 0
+  if(population == 0) {
+    fprintf(stderr, "The population cannot be 0!\n");
+    exit(EXIT_FAILURE);
+  }
+  return population;
+}
+
 // calculate the arithmetic mean of an array
 double ariMean(unsigned *dataArray, size_t totalNum) {
   double sum = 0;
@@ -58,46 +106,17 @@ country_t parseLine(char * line) {
     exit(EXIT_FAILURE);
   }
   // check whether the name length is smaller than 64
-  if((splitPointer - line) > 63) {
-    fprintf(stderr, "name length is too long!\n");
-    exit(EXIT_FAILURE);
-  }
-  
-  char* ptr = line;
-  int i = 0;
+  checkName(line, splitPointer);
+  // copy name to ans.name
   country_t ans;
-  // from the start to the splitPointer, iteratively assign the character to ans.name
-  while(ptr != splitPointer) {
-    ans.name[i] = *ptr;
-    i++;
-    ptr++;
-  }
-  // add null terminator
-  ans.name[i] = '\0';
+  char* ptr = line;
+  copyName(ptr, splitPointer, ans.name);
 
   char* newLine = strchr(line, '\n');
-  // from splitPointer to the end, iteratively check if each character is a number
-  ptr++;
-  while(ptr != newLine) {
-    if(!isNumber(*ptr)) {
-      fprintf(stderr, "The population is invalid!\n");
-      exit(EXIT_FAILURE);
-    }
-    ptr++;
-  }
+  // check if population is legal
+  checkPop(++splitPointer, newLine);
   // convert the character of population to a uint64_t type
-  ans.population = strtoul(++splitPointer, &ptr, 10);
-  // check whether the population is out of the bound
-  if(errno == ERANGE) {
-    fprintf(stderr, "The population is too large!\n");
-    exit(EXIT_FAILURE);
-  }
-  errno = 0;
-  // check whether the population is 0
-  if(ans.population == 0) {
-    fprintf(stderr, "The population cannot be 0!\n");
-    exit(EXIT_FAILURE);
-  }
+  ans.population = convertPop(splitPointer);
   return ans;
 }
 
