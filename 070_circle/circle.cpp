10ffed7d0dd056774d8213ca9935cbfeaf29edb5
ys322 <ys322@duke.edu>
2020-10-21 21:34:41 +0800

    feat: 070 submission

--- 070_circle/circle.cpp
+++ 070_circle/circle.cpp
@@ -1,7 +1,7 @@
 #include "circle.hpp"
 #include <cmath>
 
-#define PI 3.1416
+#define PI 3.1415926535
 
 Circle::Circle(Point p, const double r): center(p), radius(r) {}
 
@@ -13,15 +13,13 @@ double Circle::intersectionArea(const Circle & otherCircle) {
   double centerDis = center.distanceFrom(otherCircle.center);
   double r1 = radius;
   double r2 = otherCircle.radius;
-  if (centerDis >= r1 + r2) {
+  if (r1 == 0 || r2 == 0 || centerDis >= r1 + r2) {
     return 0;
   }
-  if (centerDis == 0) {
-    if (r1 < r2) {
-      return PI*r1*r1;
-    } else {
+  if (r2 - r1 >= centerDis) {
+    return PI*r1*r1;
+  } else {
       return PI*r2*r2;
-    }
   }
   double cosVal1 = (r1*r1 + centerDis*centerDis - r2*r2)/(2*r1*centerDis);
   double cosVal2 = (r2*r2 + centerDis*centerDis - r1*r1)/(2*r2*centerDis);
