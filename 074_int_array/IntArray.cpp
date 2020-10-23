b7e2e5c6c0410c93eb25aa7a456873001d39a909
ys322 <ys322@duke.edu>
2020-10-23 17:04:27 +0800

    feat: 074 submission

--- 074_int_array/IntArray.cpp
+++ 074_int_array/IntArray.cpp
@@ -2,36 +2,86 @@
 #include <assert.h>
 #include <ostream>
 
-IntArray::IntArray() {
-
-}
-IntArray::IntArray(int n) {
-
+IntArray::IntArray(): data(NULL), numElements(0) {
+  
 }
 
-IntArray::IntArray(const IntArray & rhs)  {
+IntArray::IntArray(int n): data(new int[n]), numElements(n) {
 
 }
+
+IntArray::IntArray(const IntArray & rhs): data(new int[rhs.numElements]), numElements(rhs.numElements) {
+  for(int i = 0; i < numElements; i++) {
+    data[i] = rhs.data[i];
+  }
+}
+
 IntArray::~IntArray() {
-
+  delete[] data;
 }
 
 IntArray & IntArray::operator=(const IntArray & rhs) {
+  if(this != &rhs) {
+    int* temp = new int[rhs.numElements];
+    for(int i = 0; i < rhs.numElements; i++) {
+      temp[i] = rhs.data[i];
+    }
+    delete[] data;
+    numElements = rhs.numElements;
+    data = temp;
+  }
+  return *this;
 }
+  
 const int & IntArray::operator[](int index) const {
+  assert(index >=0 && index < numElements);
+  return data[index];
 }
+  
 int & IntArray::operator[](int index) {
+  assert(index >= 0 && index < numElements);
+  return data[index];
 }
 
 int IntArray::size() const {
+  return numElements;
 }
 
 bool IntArray::operator==(const IntArray & rhs) const {
+  if(numElements != rhs.numElements) {
+    return false;
+  } else {
+    for(int i = 0; i < numElements; i++) {
+      if(data[i] != rhs.data[i]) {
+	return false;
+      }
+    }
+    return true;
+  }
 }
 
 bool IntArray::operator!=(const IntArray & rhs) const {
+  if(numElements != rhs.numElements) {
+    return true;
+  } else {
+    for(int i = 0; i < numElements; i++) {
+      if(data[i] != rhs.data[i]) {
+	return true;
+      }
+    }
+    return false;
+  }
 }
 
 std::ostream & operator<<(std::ostream & s, const IntArray & rhs) {
-
+  if(rhs.size() == 0) {
+    s << "{}";
+    return s;
+  }
+  s << "{";
+  for(int i = 0; i < rhs.size() - 1; i++) {
+    s << rhs[i] << ",";
+  }
+  s << rhs[rhs.size() - 1] << "}";
+  return s;
 }
