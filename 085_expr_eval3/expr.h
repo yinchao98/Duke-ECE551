c3a54b80b51e0b89b6b10a611dfb5380fc0316d1
ys322 <ys322@duke.edu>
2020-11-03 11:20:58 +0800

    feat: 085 submission

---
+++ 085_expr_eval3/expr.h
@@ -0,0 +1,98 @@
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <sstream>
+
+class Expression {
+public:
+	Expression() {}
+	virtual std::string toString() const = 0;
+	virtual long evaluate() const = 0;
+	virtual ~Expression() {}
+};
+
+class NumExpression: public Expression {
+	long number;
+public:
+	NumExpression(long n): number(n) {} 
+	virtual std::string toString() const {
+		std::stringstream outputNum;
+		outputNum << number;
+		return outputNum.str();
+	}
+	virtual long evaluate() const {
+		return number;
+	}
+	virtual ~NumExpression() {}
+};
+
+class operatorExpression: public Expression {
+protected:
+	Expression * lvalue;
+	Expression * rvalue;
+public:
+	operatorExpression(Expression * lhs, Expression * rhs): lvalue(lhs), rvalue(rhs) {}
+	virtual std::string toString() const = 0;
+	virtual long evaluate() const = 0;
+	virtual ~operatorExpression() {
+		delete lvalue;
+		delete rvalue;
+	}
+};
+
+class PlusExpression: public operatorExpression{
+public:
+	PlusExpression(Expression * lhs, Expression * rhs): operatorExpression(lhs, rhs) {}
+	virtual std::string toString() const {
+		std::stringstream exp;
+		exp << "(" << lvalue->toString() << " + " << rvalue->toString() << ")";
+		return exp.str();
+	}
+	virtual long evaluate() const {
+		return lvalue->evaluate() + rvalue->evaluate();
+	}
+	virtual ~PlusExpression() {}
+};
+
+class MinusExpression: public operatorExpression{
+public:
+	MinusExpression(Expression * lhs, Expression * rhs): operatorExpression(lhs, rhs) {}
+	virtual std::string toString() const {
+		std::stringstream exp;
+		exp << "(" << lvalue->toString() << " - " << rvalue->toString() << ")";
+		return exp.str();
+	}
+	virtual long evaluate() const {
+		return lvalue->evaluate() - rvalue->evaluate();
+	}
+	virtual ~MinusExpression() {}
+};
+
+class TimesExpression: public operatorExpression{
+public:
+	TimesExpression(Expression * lhs, Expression * rhs): operatorExpression(lhs, rhs) {}
+	virtual std::string toString() const {
+		std::stringstream exp;
+		exp << "(" << lvalue->toString() << " * " << rvalue->toString() << ")";
+		return exp.str();
+	}
+	virtual long evaluate() const {
+		return lvalue->evaluate() * rvalue->evaluate();
+	}
+	virtual ~TimesExpression() {}
+};
+
+class DivExpression: public operatorExpression{
+public:
+	DivExpression(Expression * lhs, Expression * rhs): operatorExpression(lhs, rhs) {}
+	virtual std::string toString() const {
+		std::stringstream exp;
+		exp << "(" << lvalue->toString() << " / " << rvalue->toString() << ")";
+		return exp.str();
+	}
+	virtual long evaluate() const {
+		return lvalue->evaluate() / rvalue->evaluate();
+	}
+	virtual ~DivExpression() {}
+};
+
