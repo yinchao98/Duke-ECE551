3afec44fc0ecc447c3c3aa8c6571ca48c2569137
ys322 <ys322@duke.edu>
2020-11-18 16:28:37 +0800

    feat: 103 submission

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
 
 Node *  buildTree(uint64_t * counts) {
-  //WRITE ME!
-}  
+	priority_queue_t pq;
+	for(size_t i = 0; i < 257; i++) {
+		if(counts[i] != 0) {
+			pq.push(new Node(i, counts[i]));
+		}
+	}
+	if(pq.empty()) {
+		return NULL;
+	}
+	while(pq.size() != 1) {
+		Node *node1 = pq.top();
+		pq.pop();
+		Node *node2 = pq.top();
+		pq.pop();
+		pq.push(new Node(node1, node2));
+	}
+	return pq.top();
+}
